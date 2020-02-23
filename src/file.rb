ed636eb25fa9eaf0921e60fbbbeb4c16bd182ece
Rob Thornton <rob.thornton@shopify.com>
2020-02-23 13:14:37 -0500

    Add SimpleFile class
    Since File is a built-in ruby class, SimpleFile was used instead.
    
    This class is mainly used to calculate positional information for
    tooling that need to report on the file name, line and column a
    literal is found at. There is also a mechanism for adding locations
    of newlines to support this.
    
    It will probably become necessary to add getters for the filename
    and source code.

---
+++ src/file.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# File represents a file containing source code.
+class SimpleFile
+  def add_newline(pos)
+    @new_lines << pos
+  end
+
+  def position(pos = 0)
+    line = 0
+    column = pos
+
+    new_lines.each_with_index do |line_position, index|
+      line = index
+      column = pos - line_position
+
+      break if pos < line_position
+    end
+
+    Position.new(file_name: name, line: line, column: column)
+  end
+
+  private
+
+  attr_reader :name, :new_lines
+
+  def initialize(name: '', source:, length:)
+    @name = name
+    @source = source
+    @length = length
+    @new_lines = [0]
+  end
+end
---
+++ test/file_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../src/file.rb'
+
+class TestSimpleFile < Minitest::Test
+  def test_file_position_zero
+    f = SimpleFile.new(name: 'file.smpl', source: '', length: 1)
+    position = f.position
+
+    assert_equal('file.smpl', position.file_name)
+    assert_equal(0, position.line)
+  end
+
+  def test_file_position_at_position
+    f = SimpleFile.new(name: '', source: '', length: 2)
+    position = f.position(1)
+
+    assert_equal(1, position.column)
+    assert_equal(0, position.line)
+  end
+
+  def test_file_position_with_newline
+    f = SimpleFile.new(name: '', source: '', length: 2)
+    f.add_newline(1)
+
+    position = f.position(2)
+
+    assert_equal(1, position.column)
+    assert_equal(1, position.line)
+  end
+end
