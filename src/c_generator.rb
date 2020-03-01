65ec446e325b13935b192e6b05265cac512a4e1b
Rob Thornton <rob.thornton@shopify.com>
2020-03-01 16:23:50 -0500

    Add C generator

---
+++ src/c_generator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Generates C code for the supplied program
+class CGenerator
+  def initialize(out:)
+    @out = out
+  end
+
+  def generate(_program)
+    out.puts('int main(int argc, char* argv[] { return 0; }')
+  end
+
+  private
+
+  attr_reader :out
+end
---
+++ test/c_generator_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+require_relative '../src/ast.rb'
+require_relative '../src/c_generator.rb'
+
+class CGeneratorTest < Minitest::Test
+  def test_generate_empty_program
+    file_mock = Minitest::Mock.new
+    file_mock.expect(:puts, nil, [
+                       'int main(int argc, char* argv[] { return 0; }'
+                     ])
+
+    program = Program.new
+
+    generator = CGenerator.new(out: file_mock)
+    generator.generate(program)
+
+    file_mock.verify
+  end
+end
