a059728bf0b5e7fa967b1b54a274b811c58c2039
Rob Thornton <rob.thornton@shopify.com>
2020-03-08 12:57:26 -0400

    Add typing to ast.rb

--- src/ast.rb
+++ src/ast.rb
@@ -1,14 +1,23 @@
 # typed: true
 # frozen_string_literal: true
 
+require 'sorbet-runtime'
+
 # A generic expression
 class Expression
 end
 
 # A literal value
 class Literal < Expression
-  attr_reader :literal, :position
+  extend T::Sig
 
+  sig { returns(String) }
+  attr_reader :literal
+
+  sig { returns(Integer) }
+  attr_reader :position
+
+  sig { params(literal: String, position: Integer).void }
   def initialize(literal:, position:)
     @literal = literal
     @position = position
@@ -21,12 +30,17 @@ end
 
 # A Program consists of zero or more expressions.
 class Program
+  extend T::Sig
+
+  sig { returns(T::Array[Expression]) }
   attr_reader :expressions
 
+  sig { void }
   def initialize
     @expressions = []
   end
 
+  sig { params(expression: Expression).void }
   def add_expression(expression)
     @expressions << expression
   end
