0ab836c1def20a0829fbdfd7af68e5d9f10a1b7b
Rob Thornton <rob.thornton@shopify.com>
2020-03-08 21:52:17 -0400

    Add types to c_generator.rb
    The out method and parameter should be a type which has a method
    of #puts.

--- src/c_generator.rb
+++ src/c_generator.rb
@@ -1,12 +1,17 @@
 # typed: true
 # frozen_string_literal: true
 
+require 'sorbet-runtime'
+
 # Generates C code for the supplied program
 class CGenerator
+  extend T::Sig
+
   def initialize(out:)
     @out = out
   end
 
+  sig { params(_program: Program).void }
   def generate(_program)
     out.puts('int main(int argc, char* argv[]) { return 0; }')
   end
