9bff3ba366d39c38230ddca765b4ce7bb48f50ed
Rob Thornton <rob.thornton@shopify.com>
2020-03-08 22:05:47 -0400

    Add typing to lexer.rb

--- src/lexer.rb
+++ src/lexer.rb
@@ -1,5 +1,7 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
+
+require 'sorbet-runtime'
 
 require_relative 'item.rb'
 require_relative 'scanner.rb'
@@ -7,6 +9,9 @@ require_relative 'scanner.rb'
 # Lexer is a lexical analyzer. Each call to scan will return a new
 # Token.
 class Lexer
+  extend T::Sig
+
+  sig { returns(Item) }
   def scan
     skip_whitespace
 
@@ -21,21 +26,27 @@ class Lexer
 
   private
 
-  attr_reader :file, :scanner
+  attr_reader :file
+
+  sig { returns(Scanner) }
+  attr_reader :scanner
 
   DIGITS = '0123456789'
   WHITESPACE = " \t\n\r"
   private_constant :DIGITS, :WHITESPACE
 
+  sig { returns(T::Boolean) }
   def digit?
     DIGITS.include?(scanner.character) && !eof?
   end
 
+  sig { params(file: SimpleFile).void }
   def initialize(file)
     @file = file
     @scanner = Scanner.new(file.source)
   end
 
+  sig { returns(Item) }
   def scan_integer
     start = scanner.position
     literal = ''
@@ -49,10 +60,12 @@ class Lexer
     Item.new(literal: literal, position: start, token: Token::Integer)
   end
 
+  sig { returns(T::Boolean) }
   def eof?
     scanner.character.empty?
   end
 
+  sig { void }
   def skip_whitespace
     character = scanner.character
     character = scanner.accept(WHITESPACE) until character.empty?
