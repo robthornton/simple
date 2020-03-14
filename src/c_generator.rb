3f13c9af5b13f98943a7e8d82550a34e7dccea35
Rob Thornton <rob.thornton@shopify.com>
2020-03-14 15:36:16 -0400

    Add Numeric generator

--- src/c_generator.rb
+++ src/c_generator.rb
@@ -3,22 +3,39 @@
 
 require 'sorbet-runtime'
 
+require_relative 'ast.rb'
+
 # Generates C code for the supplied program
-class CGenerator
-  extend T::Sig
+module CGenerator
+  # Program code generator
+  class Program
+    extend T::Sig
 
-  sig { params(out: T.any(File, StringIO)).void }
-  def initialize(out:)
-    @out = out
+    sig { params(out: T.any(File, StringIO)).void }
+    def initialize(out:)
+      @out = out
+    end
+
+    sig { params(_program: Ast::Program).void }
+    def generate(_program)
+      out.print('int main(int argc, char* argv[]) { return 0; }')
+    end
+
+    private
+
+    sig { returns(T.any(File, StringIO)) }
+    attr_reader :out
   end
+end
 
-  sig { params(_program: Program).void }
-  def generate(_program)
-    out.print('int main(int argc, char* argv[]) { return 0; }')
+module CGenerator
+  # Numeric literal generator
+  class Numeric
+    extend T::Sig
+
+    sig { params(out: T.any(File, StringIO), expression: NumericLiteral).void }
+    def self.generate(out:, expression:)
+      out.print(expression.literal)
+    end
   end
-
-  private
-
-  sig { returns(T.any(File, StringIO)) }
-  attr_reader :out
 end
--- test/c_generator_test.rb
+++ test/c_generator_test.rb
@@ -10,11 +10,20 @@ class CGeneratorTest < Minitest::Test
   def test_generate_empty_program
     file = StringIO.new
 
-    program = Program.new
+    program = Ast::Program.new
 
-    generator = CGenerator.new(out: file)
+    generator = CGenerator::Program.new(out: file)
     generator.generate(program)
 
     assert_equal('int main(int argc, char* argv[]) { return 0; }', file.string)
+  end
+
+  def test_generate_numeric
+    numeric = NumericLiteral.new(literal: '42', position: 0)
+    file = StringIO.new
+
+    CGenerator::Numeric.generate(out: file, expression: numeric)
+
+    assert_equal('42', file.string)
   end
 end
