a8290f1ab08d5f85de0fb8bc8763bf48041405d8
Rob Thornton <rob.thornton@shopify.com>
2020-03-14 19:12:36 -0400

    Implement binary expression in parser
    - Update typing from ignore to false
    - Implement a naive binary expression parser

--- src/parser.rb
+++ src/parser.rb
@@ -1,34 +1,51 @@
-# typed: ignore
+# typed: false
 # frozen_string_literal: true
 
 require_relative 'ast.rb'
+require_relative 'lexer.rb'
 
-# The Parser produces an abstract syntax tree
-class Parser
-  extend T::Sig
+# Simple language namespace
+module Simple
+  # The Parser produces an abstract syntax tree
+  class Parser
+    extend T::Sig
 
-  sig { returns(Ast::Program) }
-  def parse
-    item = lexer.scan
-    expression = parse_expression(item)
+    sig { returns(Ast::Program) }
+    def parse
+      item = lexer.scan
+      expression = parse_expression(item)
 
-    p = Ast::Program.new
-    p.add_expression(expression)
-    p
-  end
+      p = Ast::Program.new
+      p.add_expression(expression)
+      p
+    end
 
-  private
+    private
 
-  sig { returns(Lexer) }
-  attr_reader :lexer
+    sig { returns(Lexer) }
+    attr_reader :lexer
 
-  sig { params(lexer: Lexer).void }
-  def initialize(lexer)
-    @lexer = lexer
-  end
+    sig { params(lexer: Lexer).void }
+    def initialize(lexer)
+      @lexer = lexer
+    end
 
-  sig { params(item: Item).returns(Literal) }
-  def parse_expression(item)
-    NumericLiteral.new(literal: item.literal, position: item.position)
+    sig { params(item: Item).returns(Expression) }
+    def parse_expression(item)
+      expression = NumericLiteral.new(literal: item.literal, position: item.position)
+
+      item = lexer.scan
+      return expression if item.token == Token::EOF
+
+      parse_binary_expression(item: item, lhs: expression)
+    end
+
+    sig { params(item: Item, lhs: Expression).returns(BinaryExpression) }
+    def parse_binary_expression(item:, lhs:)
+      operator = item.literal
+      rhs = NumericLiteral.new(literal: item.literal, position: item.position)
+
+      BinaryExpression.new(lhs: lhs, operator: operator, rhs: rhs)
+    end
   end
 end
--- test/parse_test.rb
+++ test/parse_test.rb
@@ -10,10 +10,20 @@ class ParserTest < Minitest::Test
   def test_parser_integer
     file = SimpleFile.new(name: 'integer.smpl', source: '123', length: 3)
     lexer = Lexer.new(file)
-    parser = Parser.new(lexer)
+    parser = Simple::Parser.new(lexer)
     program = parser.parse
 
     assert_equal(1, program.expressions.length)
     assert_kind_of(NumericLiteral, program.expressions[0])
+  end
+
+  def test_parse_binary_expression
+    file = SimpleFile.new(name: 'add.smpl', source: '2 + 3', length: 5)
+    lexer = Lexer.new(file)
+    parser = Simple::Parser.new(lexer)
+    program = parser.parse
+
+    assert_equal(1, program.expressions.length)
+    assert_kind_of(BinaryExpression, program.expressions[0])
   end
 end
