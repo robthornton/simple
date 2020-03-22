98addea3ad732d63363ba35a1a247c598c322771
Rob Thornton <rob.thornton@shopify.com>
2020-03-22 15:00:43 -0400

    Add Simple namespace to scan item

--- lib/item.rb
+++ lib/item.rb
@@ -3,42 +3,44 @@
 
 require 'sorbet-runtime'
 
-# Token has constants represting the literal lexed.
-class Token < T::Enum
-  enums do
-    EOF = new
-    Unknown = new
-    Integer = new
-    AddOperator = new
-    SubtractOperator = new
-    MultiplyOperator = new
-    DivisionOperator = new
-    Assignment = new
-    Comma = new
-    Identifier = new
-    Var = new
+module Simple
+  # Token has constants represting the literal lexed.
+  class Token < T::Enum
+    enums do
+      EOF = new
+      Unknown = new
+      Integer = new
+      AddOperator = new
+      SubtractOperator = new
+      MultiplyOperator = new
+      DivisionOperator = new
+      Assignment = new
+      Comma = new
+      Identifier = new
+      Var = new
+    end
   end
-end
 
-# Item is a lexed literal.
-class Item
-  extend T::Sig
+  # Item is a lexed literal.
+  class Item
+    extend T::Sig
 
-  sig { returns(String) }
-  attr_reader :literal
+    sig { returns(String) }
+    attr_reader :literal
 
-  sig { returns(Integer) }
-  attr_reader :position
+    sig { returns(Integer) }
+    attr_reader :position
 
-  sig { returns(Token) }
-  attr_reader :token
+    sig { returns(Token) }
+    attr_reader :token
 
-  private
+    private
 
-  sig { params(literal: String, position: Integer, token: Token).void }
-  def initialize(literal:, position:, token:)
-    @literal = literal
-    @position = position
-    @token = token
+    sig { params(literal: String, position: Integer, token: Token).void }
+    def initialize(literal:, position:, token:)
+      @literal = literal
+      @position = position
+      @token = token
+    end
   end
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -1,78 +1,78 @@
 # typed: true
 # frozen_string_literal: true
 
-require 'minitest/autorun'
+require('minitest/autorun')
 
-require_relative '../src/item.rb'
+require('item.rb')
 
 class TestLexer < Minitest::Test
   def test_item_eof
-    item = Item.new(literal: '', position: 0, token: Token::EOF)
+    item = Simple::Item.new(literal: '', position: 0, token: Simple::Token::EOF)
 
     assert_equal('', item.literal)
     assert_equal(0, item.position)
-    assert_equal(Token::EOF, item.token)
+    assert_equal(Simple::Token::EOF, item.token)
   end
 
   def test_item_integer_literal
-    item = Item.new(literal: '123', position: 0, token: Token::Integer)
+    item = Simple::Item.new(literal: '123', position: 0, token: Simple::Token::Integer)
 
     assert_equal('123', item.literal)
     assert_equal(0, item.position)
-    assert_equal(Token::Integer, item.token)
+    assert_equal(Simple::Token::Integer, item.token)
   end
 
   def test_item_unknown_literal
-    item = Item.new(literal: '', position: 0, token: Token::Unknown)
+    item = Simple::Item.new(literal: '', position: 0, token: Simple::Token::Unknown)
 
-    assert_equal(Token::Unknown, item.token)
+    assert_equal(Simple::Token::Unknown, item.token)
   end
 
   def test_item_add_operator
-    item = Item.new(literal: '+', position: 0, token: Token::AddOperator)
+    item = Simple::Item.new(literal: '+', position: 0, token: Simple::Token::AddOperator)
 
-    assert_equal(Token::AddOperator, item.token)
+    assert_equal(Simple::Token::AddOperator, item.token)
   end
 
   def test_item_subtract_operator
-    item = Item.new(literal: '-', position: 0, token: Token::SubtractOperator)
+    item = Simple::Item.new(literal: '-', position: 0, token: Simple::Token::SubtractOperator)
 
-    assert_equal(Token::SubtractOperator, item.token)
+    assert_equal(Simple::Token::SubtractOperator, item.token)
   end
 
   def test_item_multiply_operator
-    item = Item.new(literal: '*', position: 0, token: Token::MultiplyOperator)
+    item = Simple::Item.new(literal: '*', position: 0, token: Simple::Token::MultiplyOperator)
 
-    assert_equal(Token::MultiplyOperator, item.token)
+    assert_equal(Simple::Token::MultiplyOperator, item.token)
   end
 
   def test_item_division_operator
-    item = Item.new(literal: '/', position: 0, token: Token::DivisionOperator)
+    item = Simple::Item.new(literal: '/', position: 0, token: Simple::Token::DivisionOperator)
 
-    assert_equal(Token::DivisionOperator, item.token)
+    assert_equal(Simple::Token::DivisionOperator, item.token)
   end
 
   def test_item_var_keyword
-    item = Item.new(literal: 'var', position: 0, token: Token::Var)
+    item = Simple::Item.new(literal: 'var', position: 0, token: Simple::Token::Var)
 
-    assert_equal(Token::Var, item.token)
+    assert_equal(Simple::Token::Var, item.token)
   end
 
   def test_item_identifier
-    item = Item.new(literal: 'ident', position: 0, token: Token::Identifier)
+    item = Simple::Item.new(literal: 'ident', position: 0, token: Simple::Token::Identifier)
 
-    assert_equal(Token::Identifier, item.token)
+    assert_equal(Simple::Token::Identifier, item.token)
   end
 
   def test_item_comma_separator
-    item = Item.new(literal: ',', position: 0, token: Token::Comma)
+    item = Simple::Item.new(literal: ',', position: 0, token: Simple::Token::Comma)
 
-    assert_equal(Token::Comma, item.token)
+    assert_equal(Simple::Token::Comma, item.token)
   end
 
   def test_item_assignment_operator
-    item = Item.new(literal: ':=', position: 0, token: Token::Assignment)
+    item = Simple::Item.new(literal: ':=', position: 0, token: Simple::Token::Assignment)
 
-    assert_equal(Token::Assignment, item.token)
+    assert_equal(Simple::Token::Assignment, item.token)
   end
 end
